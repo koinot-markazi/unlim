9caec5508b1f4b1812de2fdcd31d63dc1fe44e15
koinot-markazi <mahkamov4th@gmail.com>
2020-02-20 22:18:38 +0500

    fixed bugs

--- js/common.js
+++ js/common.js
@@ -12,3 +12,22 @@ $(function() {
 	  });
 
 });
+
+$('.country-list').children().click(function(){
+	$(this).siblings().removeClass('active')  	
+	$(this).addClass('active')
+})
+
+
+// Changing value of price
+$('.tarif').on("change", function(){
+	$('.big-price').html($(this).val() * 276)
+})
+
+$('.toggle-active').click(function(){
+	$('.instruction').toggle()
+})
+
+$('#number_of_periods_select').on("change", function(){
+	$('#change_period').html($(this).val()*30)
+})
